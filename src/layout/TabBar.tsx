7926078a1e2fcab38dd7f7fd6ddb920acdc7b6d6
teagueStockwell <tsAppDevelopment@gmail.com>
2021-03-17 19:46:53 -0700

    tabbar wip

--- src/layout/TabBar.tsx
+++ src/layout/TabBar.tsx
@@ -1,4 +1,3 @@
-import {ChangeEvent, useState} from 'react';
 import AppBar from '@material-ui/core/AppBar';
 import Tabs from '@material-ui/core/Tabs';
 import Tab from '@material-ui/core/Tab';
@@ -6,24 +5,13 @@ import { Link } from 'react-router-dom';
  
 
 export function TabBar() {
-  const tabIdxEP = {
-    '/':0,
-    '/add':1,
-    '/favorites':2
-  }
   return (
     <>
-      <AppBar position="fixed">
-        <Tabs value={0}>
-          <Link to='/'>
-            <Tab label="All Dogs"/>
-          </Link>
-          <Link to='/add'>
-            <Tab label="Add Dog"/>
-          </Link>
-          <Link to='/favorites'>
-            <Tab label="Favorite Dogs"/>
-          </Link>
+      <AppBar position='fixed'>
+        <Tabs>
+          <Tab label="All Dogs" component={Link} to='/'/>
+          <Tab label="Add Dog" component={Link} to='/add'/>
+          <Tab label="Favorite Dogs" component={Link} to='/favorites'/>
         </Tabs>
       </AppBar>
     </>
